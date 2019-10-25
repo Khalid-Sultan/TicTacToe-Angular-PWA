b8b3153c4a5f14c2e75916105f1350731095182e
Khalid Sultan <kalid.sultan.95@gmail.com>
2019-10-25 18:02:24 +0300

    Added Box Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoxComponent } from './box/box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoxComponent
   ],
   imports: [
     BrowserModule,
